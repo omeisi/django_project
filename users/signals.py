ecae658834073fd2ca349d340346c8b8eda73d51
Aviad <aviad339833@gmail.com>
2019-03-02 18:25:02 -0500

    hey

---
+++ users/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+#create profile to each user
+
+@receiver(post_save,sender=User)
+def create_profile(sender, instance,  created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+#save profiles 
+@receiver(post_save,sender=User)
+def save_profile(sender, instance,  created, **kwargs):
+	instance.profile.save()
